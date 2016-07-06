76351e7f6490daaaac583bc903ce2f810c92a3af
G0055490 <G0055490@CTARO2-7831.gvt.net.br>
2016-07-06 18:33:51 -0300

    New AngularJS concepts

--- webapp/resources/js/myCtrl.js
+++ webapp/resources/js/myCtrl.js
@@ -1,4 +1,28 @@
 app.controller('myCtrl', function($scope) {
 	$scope.firstName = "John";
 	$scope.lastName = "Jacques";
+	$scope.myScope = 'controller';
+
+	$scope.changeName = function() {
+		$scope.lastName = "AngularJS";
+	}
+
+	$scope.fullName = function() {
+		return $scope.firstName + " " + $scope.lastName;
+	};
+
+	$scope.names = [ {
+		name : 'Jani',
+		country : 'Norway'
+	}, {
+		name : 'Hege',
+		country : 'Sweden'
+	}, {
+		name : 'Kai',
+		country : 'Denmark'
+	} ];
+});
+
+app.run(function($rootScope) {
+	$rootScope.myScope = "root";
 });
